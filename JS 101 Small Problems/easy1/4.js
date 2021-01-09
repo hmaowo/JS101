c981c3871beaf9a6a6fb0ac4f343b6348a605a68
chibiboba <hmaowo@gmail.com>
2021-01-08 22:27:21 -0800

    more further exploration

--- JS 101 Small Problems/easy1/4.js
+++ JS 101 Small Problems/easy1/4.js
@@ -12,5 +12,47 @@ let area = length * width;
 let squareFeet = area * 10.7639; 
 
 console.log(
-  `The area of the room is ${area.toFixed(2)} square meters (${squareFeet.toFixed(2)} square feet)`
+  `The area of the room is ${area.toFixed(2)} square meters (${squareFeet.toFixed(2)} square feet).`
 );
+
+/* 
+Further Exploration
+Modify the program so that it asks the user for the input type (meters or feet). 
+Compute for the area accordingly, and log it and its conversion in parentheses. */
+
+console.log("Is your input type meters or feet?");
+let inputType = readlineSync.prompt();
+
+if (inputType === 'meters') {
+  console.log("Enter the length of the room in meters:");
+  let length = readlineSync.prompt();
+  length = parseInt(length, 10);
+
+  console.log("Enter the width of the room in meters:");
+  let width = readlineSync.prompt();
+  width = parseInt(width, 10);
+
+  let area = length * width;
+  let squareFeet = area * 10.7639;
+
+  console.log(
+    `The area of the room is ${area.toFixed(2)} square meters (${squareFeet.toFixed(2)} square feet).`
+  );
+} else if (inputType === 'feet') {
+  console.log("Enter the length of the room in feet:");
+  let length = readlineSync.prompt();
+  length = parseFloat(length);
+
+  console.log("Enter the width of the room in feet:");
+  let width = readlineSync.prompt();
+  width = parseFloat(length);
+
+  area = length * width; 
+  squareMeters = area / 10.7639;
+  console.log(
+    `The area of the room is ${area.toFixed(2)} square feet (${squareMeters.toFixed(2)} square meters).`
+  );
+} else {
+  console.log('Incorrect input type. Please enter meters or feet.');
+}
+
--- JS 101 Small Problems/easy1/6.js
+++ JS 101 Small Problems/easy1/6.js
@@ -1,4 +1,5 @@
 let rlSync = require('readline-sync');
+
 let integer = parseInt(rlSync.question("Please enter an integer greater than 0: "));
 let answer = rlSync.question(`Enter "s" to compute the sum, or "p" to compute the product. `);
 
@@ -17,3 +18,35 @@ if (answer === 's') {
 } else { 
   console.log("Oops. Unknown operation.");
 };
+
+
+/* What if the input was an array of integers instead of just a single integer? 
+How would your computeSum and computeProduct functions change? 
+Given that the input is an array, how might you make use of the Array.prototype.reduce() method? */
+
+let input = rlSync.question(`Please enter an array of integers greater than 0, separated by \
+a single empty space. Do not enclose the integers in brackets. Please folow these guidelines, \
+otherwise the function may not compute properly. 
+For example: 1 2 3 4 5 \n` );
+
+let arrayOfText = input.split(' ');
+let array = arrayOfText.map(function(element) {
+  return parseInt(element);
+}); //convert array element from string to numbers. 
+
+if (array.includes(NaN)) { 
+  console.log(`Your input did not follow the rules. Try again. `)
+} else {
+  console.log(`This is your array [${array}].`);
+  let answer = rlSync.question(`Enter "s" to compute the sum, or "p" to compute the product. `);
+
+  if (answer === 's') {
+    let sum = array.reduce((sum, element) => sum + element, 0)
+    console.log(`\nThe sum of the integers in the array is is ${sum}.`);
+  } else if (answer === 'p') {
+    let product = array.reduce((product, element) => product * element, 1); 
+    console.log(`\nThe product of the integers in the array is ${product}.`);
+  } else { 
+    console.log("Oops. Unknown operation.");
+  }
+}
