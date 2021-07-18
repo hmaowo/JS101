882e32b04065710262014f6eda6033cf06be7978
chibiboba <hmaowo@gmail.com>
2021-07-17 22:53:01 -0700

    almost finish debugging JS

---
+++ JS 101 Small Problems/Debugging JS/1.js
@@ -0,0 +1,15 @@
+let ladder = '';
+
+['head', 'heal', 'teal', 'tell', 'tall', 'tail'].forEach(word => {
+  if (ladder !== '') {
+    ladder += '-';
+  }
+
+  ladder += word;
+});
+
+console.log(ladder);  // expect: head-heal-teal-tell-tall-tail
+
+/*
+Error went away when I added semi colon to the first line
+*/
